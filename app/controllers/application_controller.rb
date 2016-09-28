1f9ccb88f29a224b6572d75927cd68c857024aa4
jdebettencourt <james.s.debettencourt@gmail.com>
2016-09-28 20:01:31 +0000

    fin toy_app

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def hello
-    render html: "<h1>Why hello there, World!</h1>"
+    render html: "Why hello there, World!"
   end
 end
