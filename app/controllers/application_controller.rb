c7fdefe1c2bc4cd2cd544cdc6b90b76aa9b673a0
jdebettencourt <james.s.debettencourt@gmail.com>
2016-10-06 15:47:23 +0000

    partially bootstrapped

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def hello
-    render html: "Why hello there, World!"
+    render html: "Why hello there, from the bootstrapped application, World!"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
+  get 'static_pages/home'
+
+  get 'static_pages/help'
+  
+  get 'static_pages/goofy'
+
   resources :microposts
   resources :users
-  root 'users#index'
+  root 'static_pages#home'
 end
